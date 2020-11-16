55f499dfbb533913930e7b178800095be751b574
charlessamoy2 <peter.samoy@obf.ateneo.edu>
2020-11-16 16:05:47 +0800

    added edit and update

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # resources :articles
+  get '/articles' => 'articles#index'
+  get '/articles/new' => 'articles#new', as: 'new_article'
+  post '/articles' => 'articles#create', as: 'create_article'
+  get '/articles/edit' => 'articles#edit', as: 'edit_article'
+  post '/articles/update' => 'articles#update', as: 'update_article'
 end
